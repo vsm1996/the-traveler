b29be308f29942ca3e98c39c6cfb259948501578
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-03-07 11:49:16 -0800

    *confused face*

--- app/api/auth/token/route.ts
+++ app/api/auth/token/route.ts
@@ -5,6 +5,6 @@ import { NextRequest, NextResponse } from "next/server";
 // AS AN EXAMPLE
 export async function GET(request: NextRequest) {
   const token = await getToken({ req: request })
-
+  console.log('hello')
   return NextResponse.json(token)
 }
