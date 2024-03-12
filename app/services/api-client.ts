2c2c7281f5e150b6eb9934c9a8a22a8f98da9e5f
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-03-12 13:52:14 -0700

    attempt to fix api on prod

--- app/services/api-client.ts
+++ app/services/api-client.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: 'http://localhost:3000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  },
   params: {
     key: process.env.NEXT_API_KEY
   }
