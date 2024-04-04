db7aeefaf6eaa3c698b8e0549808a0c4cc158047
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-04-03 21:08:05 -0700

    Add register form to register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import RegisterForm from '../components/registerForm'
 
 const RegisterPage = () => {
   return (
     <div>
       Register Page
+      <RegisterForm />
     </div>
   )
 }
