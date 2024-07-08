26080cca33e67b7bf5944c25a8762b320bad245d
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-07-07 22:12:07 -0700

    Add theme controller

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ import LatestNewsPreview from "./components/latestnews-preview";
 import Navbar from "./components/navbar";
 import TopStoriesPreview from "./components/topstories-preview";
 import AviationNewsPreview from "./components/aviationnews-preview";
+import ThemeController from "./components/themeController";
 
 export default function Home() {
   return (
     <div className={`flex min-h-screen flex-col items-center`}>
-      <h1 className="text-4xl my-4 text-orange-200">The Traveler</h1>
+      <div className="flex items-center justify-center gap-2">
+        <h1 className="text-4xl my-4 text-orange-200">The Traveler </h1>
+        <ThemeController />
+      </div>
       <div className="w-full h-full">
         <HomePageHero />
         <Navbar />
